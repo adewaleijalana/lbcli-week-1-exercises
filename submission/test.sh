417ab821b6476dfa4418b6453eaff9d9b31018b9
adewaleijalana <adewaleijalana@gmail.com>
2025-03-19 04:53:07 +0100

    getting the treasure collected updated

--- submission/test.sh
+++ submission/test.sh
@@ -103,7 +103,7 @@ NEW_BALANCE=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getbalance)
 check_cmd "New balance check"
 echo "Your treasure balance: $NEW_BALANCE BTC"
 
-COLLECTED=$((NEW_BALANCE - BALANCE))
+COLLECTED=$(awk "BEGIN {print $NEW_BALANCE - $BALANCE}")
 check_cmd "Balance calculation"
 echo "You've collected $COLLECTED BTC in treasures!"
 
